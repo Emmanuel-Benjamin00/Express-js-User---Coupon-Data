ed9b8bdd9cde1c1a0d897420b8aacf0de2706b90
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2023-10-17 02:32:48 +0530

    removed unwanted files

--- src/common/dbConfig.js
+++
@@ -1,6 +0,0 @@
-//this file is not required while using env
-
-// const dbName = 'b50wd2t'
-// const dbUrl = "mongodb+srv://emmanuel:Prince00@cluster0.gdkvrpe.mongodb.net"
-
-// module.exports = {dbName,dbUrl}
--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DB = require('../common/dbConfig')
+
 
 require('dotenv').config()
 
