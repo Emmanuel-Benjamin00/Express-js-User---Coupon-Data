8c3635867a69d0cd3fa034897989a099d7e39be2
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2023-10-18 00:20:41 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const AppRoutes = require('./src/routes')
-
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
 const PORT = process.env.PORT
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
-
 app.use('/',AppRoutes)
 
 
